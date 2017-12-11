6286c60c63856e3f30a23de9ec408b5ce9b0bda3
floriwan <nospam@goeldenitz.org>
2017-12-11 23:28:56 +0100

    export vertical speed

--- export_flight_data.lua
+++ export_flight_data.lua
@@ -1,5 +1,5 @@
 
-OUTPUT_DIR = 'C:\\Users\\fgz\\Desktop\\twitch\\'
+OUTPUT_DIR = 'C:\\Users\\Florian\\Desktop\\twitch\\'
 FL_FILE = 'flightlevel.txt'
 
 dataref("altitude_ft_pilot", "sim/cockpit2/gauges/indicators/altitude_ft_pilot")
@@ -9,15 +9,22 @@ dataref("groundspeed", "sim/flightmodel/position/groundspeed")
 old_altitude = 0;
 old_speed = 0;
 
+-- nothing todo with the export, set some shadow values
+--set("sim/private/controls/skyc/max_shadow_angle",-89) 
+--set("sim/private/controls/skyc/min_shadow_angle",-90)
+
 function dump_data()
 
 	altitude = string.format("%05d", altitude_ft_pilot)
-	vertical_speed = string.format("%03d", vvi_fpm_pilot)
+	vertical_speed = string.format("%04d", vvi_fpm_pilot)
 	speed = string.format("%03d", groundspeed * 1.94384) -- meter/sec to kts
 
 	if old_altitude ~= altitude or old_speed ~= speed then
-		out_string = "ground speed: " .. speed .. "kts  altitude: " .. altitude .. "ft"
+		
+		-- out_string = "X-Plane 11 / ground speed: " .. speed .. "kts vertical speed: " .. vertical_speed .. "ft/m altitude: " .. altitude .. "ft"
 	
+		out_string = "G/S: " .. speed .. "kts  ALT: " .. altitude .. "ft  V/S: " .. vertical_speed .. "fpm "
+		
 		alt_file = io.open(OUTPUT_DIR .. FL_FILE, 'w')
 		alt_file:write(out_string)
 		alt_file:close()
